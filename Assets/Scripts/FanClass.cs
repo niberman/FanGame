d42229debb4a199bd6e97b15cd0358e27d5467d2
Noah Berman <noahberman14@gmail.com>
2021-01-29 11:40:25 -0700

    start

---
+++ Assets/Scripts/FanClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class FanClass
+{
+    public Sprite sprite;
+    public Vector2 pos;
+    public ConstantForce forcePush;
+
+}
