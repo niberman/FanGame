0d52416ac540fad5a79d66607aa061c5cc828499
Noah Berman <noahberman14@gmail.com>
2021-02-01 21:02:09 -0700

    g

--- Assets/Scripts/FanDemo.cs
+++ Assets/Scripts/FanDemo.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class FanDemo : MonoBehaviour
 {
+    float fanStrength = 10.0f;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb == null)
+            return;
+        rb.AddForce(-transform.right * fanStrength, ForceMode2D.Impulse);
+    }
+
     
 }
