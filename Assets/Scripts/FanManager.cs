cb275acd96bbe234b20d2594b180ec00f12c5294
Noah Berman <noahberman14@gmail.com>
2021-02-08 15:08:36 -0700

    making good progress

--- Assets/Scripts/FanManager.cs
+++ Assets/Scripts/FanManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FanManager : MonoBehaviour
 {
+    public GameObject fanLeft;
+    public GameObject fanUp;
+    public GameObject fanDown;
+    public GameObject fanRight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,15 @@ public class FanManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            Instantiate(fanDown);
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+            Instantiate(fanUp);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            Instantiate(fanRight);
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            Instantiate(fanLeft);
+        else
+            return;
     }
 }
