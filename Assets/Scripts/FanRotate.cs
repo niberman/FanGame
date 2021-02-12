19a4e343824b3da1777fa4f81b703f805af61b9c
Noah Berman <noahberman14@gmail.com>
2021-02-12 11:32:26 -0700

    aaaa

---
+++ Assets/Scripts/FanRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotate : MonoBehaviour
+{
+    GameObject rotatingFan;
+    public float speed 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+    }
+}
+
