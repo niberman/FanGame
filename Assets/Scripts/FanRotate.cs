489ac0b035a35cb65d0bb0f5e3a3b84a5b6f7053
Noah Berman <noahberman14@gmail.com>
2021-02-12 13:24:44 -0700

    working

--- Assets/Scripts/FanRotate.cs
+++ Assets/Scripts/FanRotate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FanRotate : MonoBehaviour
 {
     GameObject rotatingFan;
-    public float speed 10.0f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +16,12 @@ public class FanRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.forward * 200 * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D))
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.back * 200 * Time.deltaTime);
 
     }
 }
