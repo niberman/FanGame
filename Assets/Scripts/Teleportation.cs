115184646ff286e43c3ddd312a086244ead107a2
Noah Berman <noahberman14@gmail.com>
2021-03-30 10:03:11 -0600

    good work on portals

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            StartCoroutine(Teleport());
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(0.25f);
+        player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+    }
+}
